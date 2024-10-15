aeb621f73b1b92d6f51b9591f9d7b0456fc7db14
bhargavnama <bhargavnama143@gmail.com>
2024-10-15 18:27:37 +0530

    Completed QR Code GEnerator Project...

---
+++ app.js
@@ -0,0 +1,17 @@
+let qrText = document.querySelector("#qrText");
+let imgBox = document.querySelector("#imgBox");
+let qrImg = document.querySelector("#qrImg");
+let generate = document.querySelector("#generate");
+
+generate.addEventListener("click", () => {
+    if(qrText.value.length > 0){
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+        imgBox.classList.add("show-img");
+    }
+    else{
+        qrText.classList.add("error");
+        setTimeout(() => {
+            qrText.classList.remove("error");
+        }, 1000);
+    }
+})
